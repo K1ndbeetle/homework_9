29587790ec967aff5fdd264df9c11f26f28a151d
K1ndbeetle <alx-s@bk.ru>
2022-12-12 16:17:13 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
         System.out.println(" ");
+
+        //task №2
+        System.out.println("Задача №2");
+        String NameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + NameUpper);
+        System.out.println();
     }
 }
